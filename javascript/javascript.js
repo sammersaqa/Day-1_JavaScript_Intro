9aee5dd38fb4f9a368f2def0272eb9d83db65128
sammersaqa <sammersaqa@gmail.com>
2025-11-10 10:23:56 -0800

    sams comment

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -32,3 +32,5 @@ console.log( myArray );
 let myElement = document.getElementById("myElement");
 console.log(myElement);
 myElement.textContent = "I changed the text from JS! ";
+
+// This is a comment in my app js on sams branch
